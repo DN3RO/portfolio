aa0454392057e102667ab3a2d9fddcfb1d25b546
DN3RO <nirsaias095@gmail.com>
2024-06-14 22:59:26 +0300

    Updates

---
+++ precache-manifest.bbd7c60a4bd3bdcd337eb793c750c6bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbef6f233d8a4b30baeaaac50b4e0962",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "53c4362ff34289eb8efd",
+    "url": "/portfolio/static/css/2.75bc9036.chunk.css"
+  },
+  {
+    "revision": "53c4362ff34289eb8efd",
+    "url": "/portfolio/static/js/2.d35480e5.chunk.js"
+  },
+  {
+    "revision": "614472a3cf6f0d6ff951da127c0792a4",
+    "url": "/portfolio/static/js/2.d35480e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ed74b6746feaa62b42",
+    "url": "/portfolio/static/js/main.cf025001.chunk.js"
+  },
+  {
+    "revision": "3d5b06699ea436b1849a",
+    "url": "/portfolio/static/js/runtime-main.f975e493.js"
+  },
+  {
+    "revision": "ff61d2860bde30a53cf695af9664ddd7",
+    "url": "/portfolio/static/media/GH.ff61d286.svg"
+  },
+  {
+    "revision": "9228dbd1df0d6727d003942e96bf11c1",
+    "url": "/portfolio/static/media/defaultNavLogo.9228dbd1.svg"
+  },
+  {
+    "revision": "6c7d72e66a546a8ca8c54c5956becb50",
+    "url": "/portfolio/static/media/hero-dark.6c7d72e6.jpg"
+  },
+  {
+    "revision": "4cb9e4a324c6803f7ae1d160a712bbd1",
+    "url": "/portfolio/static/media/hero-light.4cb9e4a3.jpg"
+  },
+  {
+    "revision": "8a166442456e24843563657c9c46d83d",
+    "url": "/portfolio/static/media/logo.8a166442.svg"
+  }
+]);
